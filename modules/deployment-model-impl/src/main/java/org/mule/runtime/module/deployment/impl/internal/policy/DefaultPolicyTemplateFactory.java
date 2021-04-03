b29d2627d61938d7bc2a6068294d9396f2cff73b
Ivan Fritzler <ifritzler@mulesoft.com>
2021-04-03 20:02:08 -0300

    Adding feature flag check and restoring previous behavior

--- modules/deployment-model-impl/src/main/java/org/mule/runtime/module/deployment/impl/internal/policy/DefaultPolicyTemplateFactory.java
+++ modules/deployment-model-impl/src/main/java/org/mule/runtime/module/deployment/impl/internal/policy/DefaultPolicyTemplateFactory.java
@@ -93,6 +93,7 @@ public class DefaultPolicyTemplateFactory implements PolicyTemplateFactory {
     validateArtifactLicense(policyClassLoader.getClassLoader(), artifactPlugins, licenseValidator);
 
     return new DefaultPolicyTemplate(policyClassLoader.getArtifactId(), descriptor, policyClassLoader,
+                                     artifactPlugins,
                                      resolveOwnArtifactPlugins(artifactPlugins, ownResolvedPluginDescriptors,
                                                                ownPolicyClassLoader));
   }
